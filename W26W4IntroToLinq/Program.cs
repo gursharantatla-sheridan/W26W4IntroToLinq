030117b33480b124e0c6dbda5c4ba4992d10d308
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-01-30 11:25:45 -0500

    complete

--- W26W4IntroToLinq/Program.cs
+++ W26W4IntroToLinq/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace W26W4IntroToLinq
+﻿using ConsoleTables;
+
+namespace W26W4IntroToLinq
 {
     internal class Program
     {
@@ -50,7 +52,71 @@
             // deferred execution
             foreach (var i in startsWithR)
                 Console.WriteLine(i);
-            Console.WriteLine("\n");
+            Console.WriteLine("\n\n\n");
+
+
+
+
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee("James", "White", 4000),
+                new Employee("Anne", "Indigo", 5000),
+                new Employee("Bob", "Greene", 3000),
+                new Employee("James", "Indigo", 6000),
+                new Employee("Alice", "Indigo", 5500),
+                new Employee("Sam", "Brown", 7000)
+            };
+
+            foreach (var emp in employees)
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+            var between4k6k = from e in employees
+                              where e.Salary >= 4000 && e.Salary <= 6000
+                              select e;
+
+            foreach (var emp in between4k6k)
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+            var sortedEmps = from e in employees
+                             orderby e.LastName, e.FirstName
+                             select e;
+
+            foreach (var emp in sortedEmps)
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+            var lastnames = from e in employees
+                            select e.LastName;
+
+            foreach (var emp in lastnames.Distinct())
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+            var empNames = from e in employees
+                           select new { e.FirstName, e.LastName };
+
+            foreach (var emp in empNames)
+                Console.WriteLine(emp);
+            Console.WriteLine("\n\n");
+
+
+
+            // ConsoleTables example
+            ConsoleTable tbl = new ConsoleTable("First Name", "Last Name", "Salary");
+
+            foreach (var e in employees)
+            {
+                tbl.AddRow(e.FirstName, e.LastName, e.Salary.ToString("C"));
+            }
+
+            tbl.Write(Format.MarkDown);
+            Console.WriteLine();
         }
     }
 }
